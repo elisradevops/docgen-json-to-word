51d9b65e2c63c296120ce6ba27c716efe22052b5
Eden Zvi Schwartz <eden@korentec.co.il>
2025-05-20 11:25:30 +0300

    #301 New fields support for Test Reporter
    - Add support for new Associated CRs, subsystem and assigned to fields

---
+++ src/Models/TestReporterModels/AssociatedCRModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonToWord.Models.TestReporterModels
+{
+    public class AssociatedCRModel
+    {
+        public string Id { get; set; }
+        public string crTitle { get; set; }
+        public string Url { get; set; }
+    }
+}
