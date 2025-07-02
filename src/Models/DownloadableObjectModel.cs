51f0a2e187f0157f69bba8b03da9a6db9ab5ee29
Eden Zvi Schwartz <eden@korentec.co.il>
2025-07-02 17:00:22 +0300

    #321 Add Ability to download the generated SVD document
    - Add support for downloading more easily the generated document

---
+++ src/Models/DownloadableObjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonToWord.Models
+{
+    public class DownloadableObjectModel
+    {
+        public string FileName { get; set; }
+        public string Base64 { get; set; }
+        public string ApplicationType { get; set; }
+    }
+}
