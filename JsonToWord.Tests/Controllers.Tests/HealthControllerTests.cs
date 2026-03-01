89a13e6cc07e579fab16164c1c10500c5af39f23
Eden Zvi Schwartz <eden@korentec.co.il>
2026-03-01 13:49:12 +0200

    add more tests

--- JsonToWord.Tests/Controllers.Tests/HealthControllerTests.cs
+++ JsonToWord.Tests/Controllers.Tests/HealthControllerTests.cs
@@ -1,6 +1,9 @@
 using JsonToWord.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Globalization;
 
 namespace JsonToWord.Controllers.Tests
 {
@@ -21,6 +24,33 @@ namespace JsonToWord.Controllers.Tests
             Assert.Contains("\"connectionStatus\":\"connected\"", json);
             Assert.Contains("\"version\":\"", json);
             Assert.Contains("\"timestamp\":\"", json);
+        }
+
+        [Fact]
+        public void GetHealth_ReturnsIsoUtcTimestampAndNonEmptyVersion()
+        {
+            var controller = new HealthController();
+
+            var result = controller.GetHealth();
+
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var json = JsonConvert.SerializeObject(ok.Value);
+            var payload = JObject.Parse(json);
+
+            var version = payload["version"]?.Value<string>();
+            Assert.False(string.IsNullOrWhiteSpace(version));
+
+            var timestampText = payload["timestamp"]?.Value<string>();
+            Assert.False(string.IsNullOrWhiteSpace(timestampText));
+            Assert.True(
+                DateTime.TryParse(
+                    timestampText,
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.AllowWhiteSpaces,
+                    out var parsedTimestamp
+                )
+            );
+            Assert.True(parsedTimestamp > DateTime.MinValue);
         }
     }
 }
